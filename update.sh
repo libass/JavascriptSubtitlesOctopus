81e08b6ee986d5ccfb023cea4410b7642bec38f9
Dador <dldador@gmail.com>
2017-10-20 01:46:04 +0300

    Update examples.

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 cp ../JavascriptSubtitlesOctopus/js/subtitles-octopus.js subtitles-octopus.js
 cp ../JavascriptSubtitlesOctopus/js/subtitles-octopus-worker.* .
 cp ../JavascriptSubtitlesOctopus/example/*.html .
+cp ../JavascriptSubtitlesOctopus/example/*.ass .
+cp ../JavascriptSubtitlesOctopus/example/fonts/ . -R
 find *.html -type f -print0 | xargs -0 sed -i 's/\/js\//\/JavascriptSubtitlesOctopus\//g'
 find *.html -type f -print0 | xargs -0 sed -i 's/\/example\//\/JavascriptSubtitlesOctopus\//g'
