6fd7ae239a35a30e80b9f70ddd55e7425a57b546
Vasily <just.one.man@yandex.ru>
2020-01-23 13:33:20 +0300

    Add polyfill for String.endsWith

--- src/pre-worker.js
+++ src/pre-worker.js
@@ -1,4 +1,14 @@
 //var Module = Module || {};
+
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
 Module = Module || {};
 
 Module["preRun"] = Module["preRun"] || [];
