149e01585a401effd73630aad7cc1a0dad95c231
Vasily <just.one.man@yandex.ru>
2020-03-16 14:20:25 +0300

    Implement manual blending in C

--- src/SubtitleOctopus.cpp
+++ src/SubtitleOctopus.cpp
@@ -166,6 +166,120 @@ public:
     }
 };
 
+#define CLAMP_UINT8(value) ((value > 0) ? ((value <= 255) ? (int)value : 255) : 0)
+
+void* libassjs_render_blend(double tm, int force, int *dest_x, int *dest_y, int *dest_width, int *dest_height)
+{
+    int changed;
+    ASS_Image *img = ass_render_frame(ass_renderer, track, (int)(tm * 1000), &changed);
+    if (img == NULL || (changed == 0 && !force))
+    {
+        return NULL;
+    }
+
+    // find bounding rect first
+    int min_x = img->dst_x, min_y = img->dst_y;
+    int max_x = img->dst_x + img->w - 1, max_y = img->dst_y + img->h - 1;
+    ASS_Image *cur;
+    for (cur = img->next; cur != NULL; cur = cur->next)
+    {
+        if (cur->dst_x < min_x) min_x = cur->dst_x;
+        if (cur->dst_y < min_y) min_y = cur->dst_y;
+        int right = cur->dst_x + cur->w - 1;
+        int bottom = cur->dst_y + cur->h - 1;
+        if (right > max_x) max_x = right;
+        if (bottom > max_y) max_y = bottom;
+    }
+
+    // make float buffer for blending
+    int width = max_x - min_x + 1, height = max_y - min_y + 1;
+    float* buf = (float*)malloc(sizeof(float) * width * height * 4);
+    if (buf == NULL)
+    {
+        printf("libass: error: cannot allocate buffer for blending");
+        return NULL;
+    }
+    unsigned char *result = (unsigned char*)malloc(sizeof(unsigned char) * width * height * 4);
+    if (result == NULL)
+    {
+        printf("libass: error: cannot allocate result for blending");
+        free(buf);
+        return NULL;
+    }
+    memset(buf, 0, sizeof(float) * width * height * 4);
+    memset(result, 0, sizeof(unsigned char) * width * height * 4);
+
+    // blend things in
+    for (cur = img; cur != NULL; cur = cur->next)
+    {
+        int curw = cur->w, curh = cur->h;
+        if (curw == 0 || curh == 0) continue; // skip empty images
+        int curs = cur->stride, curx = cur->dst_x, cury = cur->dst_y;
+        
+        unsigned char *bitmap = cur->bitmap;
+        float a = (cur->color & 0xFF) / 255.0;
+
+        // premultiply by alpha
+        float r = a * ((cur->color >> 24) & 0xFF) / 255.0;
+        float g = a * ((cur->color >> 16) & 0xFF) / 255.0;
+        float b = a * ((cur->color >> 8) & 0xFF) / 255.0;
+
+        // float a = (255 - (cur->color & 0xFF)) / 255.0;
+
+        for (int y = 0, bitmap_offset = 0; y < curh; y++, bitmap_offset += curs)
+        {
+            int buf_line_coord = (cury + y) * width;
+            for (int x = 0; x < curw; x++)
+            {
+                float pix_alpha = bitmap[bitmap_offset + x] * a / 255.0;
+                float inv_alpha = 1.0 - pix_alpha;
+                
+                int buf_coord = (buf_line_coord + curx + x) << 2;
+                float *buf_r = buf + buf_coord;
+                float *buf_g = buf + buf_coord + 1;
+                float *buf_b = buf + buf_coord + 2;
+                float *buf_a = buf + buf_coord + 3;
+                
+                float buf_alpha = *buf_a;
+                
+                // do the compositing
+                *buf_a = pix_alpha + buf_alpha * inv_alpha;
+                *buf_r = r + *buf_r * inv_alpha;
+                *buf_g = g + *buf_g * inv_alpha;
+                *buf_b = b + *buf_b * inv_alpha;
+            }
+        }
+    }
+    
+    // now build the result
+    for (int y = 0, buf_line_coord = 0; y < height; y++, buf_line_coord += width)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int buf_coord = (buf_line_coord + x) << 2;
+            // need to un-multiply the result
+            float alpha = buf[buf_coord + 3];
+            if (alpha > 1e-5)
+            {
+                for (int offset = 0; offset < 3; offset++)
+                {
+                    float value = buf[buf_coord + offset] / alpha;
+                    result[buf_coord + offset] = CLAMP_UINT8(value);
+                }
+                result[buf_coord + 3] = CLAMP_UINT8(alpha);
+            }
+        }
+    }
+    
+    // return the thing
+    free(buf);
+    *dest_x = min_x;
+    *dest_y = min_y;
+    *dest_width = width;
+    *dest_height = height;
+    return result;
+}
+
 int main(int argc, char *argv[]) {}
 
 #include "./SubOctpInterface.cpp"
