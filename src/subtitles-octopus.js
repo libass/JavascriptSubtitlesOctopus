f54f72ab18dbdda137804c81bba986345d0f9951
Vasily <just.one.man@yandex.ru>
2020-04-27 18:28:28 +0300

    Reduce spam in logs when worker is not yet initialized
    Cherry-picked from: https://github.com/jellyfin/JavascriptSubtitlesOctopus/commit/595d0e20e58634130c237a2853be70fa193c530d

--- src/subtitles-octopus.js
+++ src/subtitles-octopus.js
@@ -335,7 +335,9 @@ var SubtitlesOctopus = function (options) {
                     iteration: self.oneshotState.iteration
                 });
             } else {
-                console.info('worker busy, requesting to seek');
+                if (self.workerActive) {
+                    console.info('worker busy, requesting to seek');
+                }
                 self.oneshotState.requestNextTimestamp = lastRendered;
             }
         }
