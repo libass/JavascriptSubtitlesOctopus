4ef6bbd9d0fb4c13a1a8950aad028afd315581b4
Dmitry Lyzo <ashephard0@gmail.com>
2021-10-02 22:07:18 +0300

    Move the polyfills to a separate file

--- Makefile
+++ Makefile
@@ -325,9 +325,10 @@ dist/js/subtitles-octopus-worker.js: src/subtitles-octopus-worker.bc src/pre-wor
 		-s WASM=1 \
 		$(EMCC_COMMON_ARGS)
 
-dist/js/subtitles-octopus-worker-legacy.js: src/subtitles-octopus-worker.bc src/pre-worker.js src/SubOctpInterface.js src/post-worker.js build/lib/brotli/js/decode.js
+dist/js/subtitles-octopus-worker-legacy.js: src/subtitles-octopus-worker.bc src/polyfill.js src/pre-worker.js src/SubOctpInterface.js src/post-worker.js build/lib/brotli/js/decode.js
 	mkdir -p dist/js
 	emcc src/subtitles-octopus-worker.bc $(OCTP_DEPS) \
+		--pre-js src/polyfill.js \
 		--pre-js src/pre-worker.js \
 		--pre-js build/lib/brotli/js/decode.js \
 		--post-js src/SubOctpInterface.js \
---
+++ src/polyfill.js
@@ -0,0 +1,67 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (search, pos) {
+        if (pos === undefined) {
+            pos = 0;
+        }
+        return this.substring(pos, search.length) === search;
+    };
+}
+
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function (search, this_len) {
+        if (this_len === undefined || this_len > this.length) {
+            this_len = this.length;
+        }
+        return this.substring(this_len - search.length, this_len) === search;
+    };
+}
+
+if (!String.prototype.includes) {
+    String.prototype.includes = function (search, pos) {
+        return this.indexOf(search, pos) !== -1;
+    };
+}
+
+if (!Uint8Array.prototype.slice) {
+    Object.defineProperty(Uint8Array.prototype, 'slice', {
+        value: function (begin, end)
+            {
+                return new Uint8Array(this.subarray(begin, end));
+            }
+    });
+}
+
+if (!Int16Array.from) {
+    // Doesn't work for String
+    Int16Array.from = function (source) {
+        var arr = new Int16Array(source.length);
+        arr.set(source, 0);
+        return arr;
+    };
+}
+
+if (!Int32Array.from) {
+    // Doesn't work for String
+    Int32Array.from = function (source) {
+        var arr = new Int32Array(source.length);
+        arr.set(source, 0);
+        return arr;
+    };
+}
+
+// performance.now() polyfill
+if ("performance" in self === false) {
+    self.performance = {};
+}
+Date.now = (Date.now || function () {
+    return new Date().getTime();
+});
+if ("now" in self.performance === false) {
+    var nowOffset = Date.now();
+    if (performance.timing && performance.timing.navigationStart) {
+        nowOffset = performance.timing.navigationStart
+    }
+    self.performance.now = function now() {
+        return Date.now() - nowOffset;
+    }
+}
