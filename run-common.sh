b0e1fa99712b8e2a24ba8f1c0b04423c152be052
Thiago França da Silva <tfsthiagobr98@outlook.com>
2021-10-02 11:17:07 -0300

    build: fix invalid default container name for docker

--- run-common.sh
+++ run-common.sh
@@ -2,7 +2,7 @@ usage() {
     echo "$0 [-f] [-c con_name] [-i img_name] [commmand [command_args...]]"
     echo "  -f: Skip building the container and reuse existing (\"fast\")"
     echo "  -c: Name of the container to create/use;"
-    echo "      defaults to libass/javascriptsubtitlesoctopus-build"
+    echo "      defaults to libass_javascriptsubtitlesoctopus-build"
     echo "  -i: Name of the image to buld/use;"
     echo "      defaults to libass/javascriptsubtitlesoctopus-build"
     echo "If no command is given `make` without arguments will be executed"
@@ -10,7 +10,7 @@ usage() {
 }
 
 OPTIND=1
-CONTAINER="libass/javascriptsubtitlesoctopus-build"
+CONTAINER="libass_javascriptsubtitlesoctopus-build"
 IMAGE="libass/javascriptsubtitlesoctopus-build"
 FAST=0
 while getopts "fc:s:" opt ; do
