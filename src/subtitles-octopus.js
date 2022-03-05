13b96a51f2aae617255d1a2018e47b4af303752c
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-03-05 01:40:37 +0100

    add requestVideoFrameCallback polyfill

--- src/subtitles-octopus.js
+++ src/subtitles-octopus.js
@@ -582,3 +582,40 @@ export default class SubtitlesOctopus extends EventTarget {
 if (typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports) {
   exports = module.exports = SubtitlesOctopus
 }
+
+if (!('requestVideoFrameCallback' in HTMLVideoElement.prototype) && 'getVideoPlaybackQuality' in HTMLVideoElement.prototype) {
+  HTMLVideoElement.prototype._rvfcpolyfillmap = {}
+  HTMLVideoElement.prototype.requestVideoFrameCallback = function (callback) {
+    const quality = this.getVideoPlaybackQuality()
+    const baseline = this.mozPaintedFrames || quality.totalVideoFrames - quality.droppedVideoFrames
+
+    const check = () => {
+      const newquality = this.getVideoPlaybackQuality()
+      const current = this.mozPaintedFrames || newquality.totalVideoFrames - newquality.droppedVideoFrames
+      if (current > baseline) {
+        const now = performance.now()
+        callback(now, {
+          presentationTime: now,
+          expectedDisplayTime: now + (this.mozFrameDelay / 1000 || 0),
+          width: this.videoWidth,
+          height: this.videoHeight,
+          mediaTime: this.currentTime,
+          presentedFrames: current,
+          processingDuration: this.mozFrameDelay || (newquality.totalFrameDelay - quality.totalFrameDelay) || 0
+        })
+        delete this._rvfcpolyfillmap[handle]
+      } else {
+        this._rvfcpolyfillmap[handle] = requestAnimationFrame(check)
+      }
+    }
+
+    const handle = Date.now()
+    this._rvfcpolyfillmap[handle] = requestAnimationFrame(check)
+    return handle
+  }
+
+  HTMLVideoElement.prototype.cancelVideoFrameCallback = function (handle) {
+    cancelAnimationFrame(this._rvfcpolyfillmap[handle])
+    delete this._rvfcpolyfillmap[handle]
+  }
+}
