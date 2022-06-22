libass/JavascriptSubtitlesOctopus

commit 9b1fc2ebb5857cae8648a11bfe84640dffe291df

make: split global linker and compile flags
Future emscripten versions warn if link-only options
are set during compilation.

Makefile
@@ -4,7 +4,9 @@
 BASE_DIR:=$(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 DIST_DIR:=$(BASE_DIR)dist/libraries
 
-GLOBAL_CFLAGS:=-O3 -s ENVIRONMENT=web,webview
+GLOBAL_CFLAGS:=-O3
+GLOBAL_LDFLAGS:=-s ENVIRONMENT=web,webview
+export LDFLAGS = $(GLOBAL_LDFLAGS)
 
 all: subtitleoctopus
 
@@ -158,7 +160,6 @@ $(DIST_DIR)/lib/libharfbuzz.a: build/lib/freetype/build_hb/dist_hb/lib/libfreety
 		-s NO_EXIT_RUNTIME=1 \
 		-s MODULARIZE=1 \
 		" \
-		LDFLAGS="" \
 		--prefix="$(DIST_DIR)" \
 		--host=x86-none-linux \
 		--build=x86_64 \
@@ -292,7 +293,7 @@ src/subtitles-octopus-worker.bc: $(OCTP_DEPS) src/Makefile src/SubtitleOctopus.c
 
 # Dist Files
 EMCC_COMMON_ARGS = \
-	$(GLOBAL_CFLAGS) \
+	$(GLOBAL_LDFLAGS) \
 	-s EXPORTED_FUNCTIONS="['_main', '_malloc']" \
 	-s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'getValue', 'FS_createPreloadedFile', 'FS_createPath']" \
 	-s NO_EXIT_RUNTIME=1 \

commit 952afbd2f228523fa4c57895b4e7002dfd881be0

make: also specify optimisation level for linking
With emscripten, LLVM is translated to JS during linking
and the optimisation level also has an effect here.
This was a regression from 9b1fc2ebb5857cae8648a11bfe84640dffe291df
and apart from performance also brings the total size of the distributed
files back down to ~7MiB from ~19MiB after the options were split.

Makefile
@@ -4,7 +4,7 @@
 BASE_DIR:=$(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 DIST_DIR:=$(BASE_DIR)dist/libraries
 
-export LDFLAGS = -s ENVIRONMENT=web,webview,worker -s NO_EXIT_RUNTIME=1
+export LDFLAGS = -O3 -s ENVIRONMENT=web,webview,worker -s NO_EXIT_RUNTIME=1
 export CFLAGS = -O3 -s USE_PTHREADS=0
 export CXXFLAGS = $(CFLAGS)
 export PKG_CONFIG_PATH = $(DIST_DIR)/lib/pkgconfig