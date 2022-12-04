bef0db39b13f36ffa7f47ca61dbe6d71bedb32b8
rcombs <rcombs@rcombs.me>
2022-12-03 19:40:02 -0600

    run-docker-build: remove -it flag; fixes npm publish

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -8,7 +8,7 @@ if [ "$FAST" -eq 0 ] ; then
     docker build -t "$IMAGE" .
 fi
 if [ "$#" -eq 0 ] ; then
-    docker run -it --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest
+    docker run --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest
 else
-    docker run -it --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest "$@"
+    docker run --rm -v "${PWD}":/code --name "$CONTAINER" "$IMAGE":latest "$@"
 fi
